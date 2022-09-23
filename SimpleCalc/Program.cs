f11575f0d7c4838ca18077139cb70f2b958c3532
IrinaMatisik <matisik_dist@mail.ru>
2022-09-23 12:11:48 +0300

    sinus

---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double resAdd = a + b;
+            Console.WriteLine("Result addition = " + resAdd);
+            double resSub = a - b;
+            Console.WriteLine("Result subtraction = " + resSub);
+            double resMul = a * b;
+            Console.WriteLine("Result subtraction = " + resMul);
+            double resDiv = a / b;
+            Console.WriteLine("Result division = " + resDiv);
+            double resSin = Math.Sin(a);
+            Console.WriteLine("Result sinus = " + resSin);
+            Console.ReadKey();
+        }
+    }
+}
